f376e005e7f548ebb6ae0c73d3d3627d04a28a43
GauravAthode <gauravathode123@gmail.com>
2025-11-23 16:24:27 +0530

    nov23

---
+++ SirAssignment/Calculator/script.js
@@ -0,0 +1,12 @@
+function Input(char) {
+  if (char === "=") {
+    const exp = document.getElementById("display").value;
+    document.getElementById("display").value = eval(exp);
+  } else if (char === "c") {
+    document.getElementById("display").value = "";
+  } else {
+    let exp = document.getElementById("display").value;
+    exp = exp + char;
+    document.getElementById("display").value = exp;
+  }
+}
